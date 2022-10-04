5ebcd96d7ebb71f377fd384a120922c3e5209c67
Swapnil Sawat <swapnil.siddheshwar@hexaviewtech.com>
2022-10-04 17:29:23 +0530

    updated task

--- Employee_CRUD/SQLQueriesUsed/Employee_CRUD_queries.sql
+++ Employee_CRUD/SQLQueriesUsed/Employee_CRUD_queries.sql
@@ -1,39 +1,51 @@
+use Employee_Data;
 Create table Employees(
     Id int IDENTITY(1,1) PRIMARY KEY,
     [Name] Varchar(60) Not Null,
     Department Varchar(30) Not Null
 );
 
+select * from Employees;
 
-Create procedure AddEmployee
+Alter procedure AddEmployee
 @Name varchar(60),
-@Department varchar(30)
+@Department varchar(30),
+@EmployeeId int Out,
+@EmployeeName varchar(60) Out,
+@EmployeeDepartment varchar(30) Out
 AS
 BEGIN
-Insert into Employees([Name],Department)
-Values(@Name,@Department)
+Insert into Employees([Name],Department) Values(@Name,@Department)
+SELECT @EmployeeId = SCOPE_IDENTITY()
+SELECT @EmployeeName = [Name], @EmployeeDepartment = Department FROM Employees Where Id = @EmployeeId
 END
 
-
-Create procedure EditEmployee
+Alter procedure EditEmployee
 @Id int,
 @Name varchar(60),
-@Department varchar(30)
+@Department varchar(30),
+@EmployeeId int Out,
+@EmployeeName varchar(60) Out,
+@EmployeeDepartment varchar(30) Out
 AS
 BEGIN
 Update Employees
 Set [Name] = @Name, Department = @Department
 Where Id = @Id
+SELECT @EmployeeId = Id, @EmployeeName = [Name], @EmployeeDepartment = Department FROM Employees Where Id = @Id
 END
 
-
-Create procedure DeleteEmployee
-@Id int
+Alter procedure DeleteEmployee
+@Id int,
+@EmployeeId int Out,
+@EmployeeName varchar(60) Out,
+@EmployeeDepartment varchar(30) Out
 AS
 BEGIN
+SELECT @EmployeeId = @Id
+SELECT @EmployeeName = [Name], @EmployeeDepartment = Department FROM Employees Where Id = @EmployeeId
 Delete from Employees Where Id = @Id
 END
-
 
 Create procedure GetAllEmployees
 AS
